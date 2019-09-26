f104bd39282c1c4041e73d5516dcf5a382145f7f
tlfobe <lennyfobe@gmail.com>
2019-09-26 01:07:38 -0600

    added test for all incorrect types

--- get_data.py
+++ get_data.py
@@ -1,6 +1,7 @@
 
 def read_stdin_col(col_num):
     if col_num is None:
-        raise TypeError("read_stdin_col : Please provide a column number")
-
+        raise TypeError("read_stdin_col : Please provide a column number!")
+    if not isinstance(col_num, int):
+        raise TypeError("read_stdin_col : Incorrect input type!")
     return None
--- test_get_data.py
+++ test_get_data.py
@@ -4,4 +4,10 @@ import get_data
 class TestGetData(unittest.TestCase):
     def test_read_stdin_null(self):
         self.assertRaises(TypeError, get_data.read_stdin_col, None)
-        
+
+    def test_read_stdin_incorrect_types(self):
+        self.assertRaises(TypeError, get_data.read_stdin_col, [1,2,3])
+        self.assertRaises(TypeError, get_data.read_stdin_col, "string")
+        self.assertRaises(TypeError, get_data.read_stdin_col, {1:2, 2:3})
+        self.assertRaises(TypeError, get_data.read_stdin_col, 10.32)
+        self.assertRaises(TypeError, get_data.read_stdin_col, 1+1j)

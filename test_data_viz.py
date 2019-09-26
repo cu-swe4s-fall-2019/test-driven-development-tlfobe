57dafcb8947fc17acd656e3b9b7e5295315b60f8
tlfobe <lennyfobe@gmail.com>
2019-09-26 17:39:20 -0600

    added pycode dependency, added wrong type file input erro

--- test_data_viz.py
+++ test_data_viz.py
@@ -38,13 +38,17 @@ class TestBoxPlot(unittest.TestCase):
         self.assertTrue(os.path.exists("newfile.png"))
         os.remove("newfile.png")
 
-    def test_combo_permission(self):
+    def test_boxplot_permission(self):
         with open("read_only.png", "w") as f:
             f.write("Hello, World!")
         os.chmod("read_only.png", stat.S_IREAD | stat.S_IRGRP | stat.S_IROTH)
         self.assertRaises(PermissionError, data_viz.boxplot, [
                           1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1], "read_only.png")
         os.remove("read_only.png")
+
+    def test_boxplot_wrong_file(self):
+        self.assertRaises(TypeError, data_viz.boxplot,
+                          [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1], "read_only.txt")
 
 
 class TestHistogram(unittest.TestCase):
@@ -81,13 +85,17 @@ class TestHistogram(unittest.TestCase):
         self.assertTrue(os.path.exists("newfile.png"))
         os.remove("newfile.png")
 
-    def test_combo_permission(self):
+    def test_histogram_permission(self):
         with open("read_only.png", "w") as f:
             f.write("Hello, World!")
         os.chmod("read_only.png", stat.S_IREAD | stat.S_IRGRP | stat.S_IROTH)
         self.assertRaises(PermissionError, data_viz.histogram, [
                           1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1], "read_only.png")
         os.remove("read_only.png")
+
+    def test_histogram_wrong_file(self):
+        self.assertRaises(TypeError, data_viz.histogram,
+                          [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1], "read_only.txt")
 
 
 class TestCombo(unittest.TestCase):
@@ -128,6 +136,10 @@ class TestCombo(unittest.TestCase):
         with open("read_only.png", "w") as f:
             f.write("Hello, World!")
         os.chmod("read_only.png", stat.S_IREAD | stat.S_IRGRP | stat.S_IROTH)
-        self.assertRaises(PermissionError, data_viz.combo, [
-                          1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1], "read_only.png")
+        self.assertRaises(PermissionError, data_viz.combo,
+                          [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1], "read_only.png")
         os.remove("read_only.png")
+
+    def test_combo_wrong_file(self):
+        self.assertRaises(TypeError, data_viz.combo,
+                          [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1], "read_only.txt")

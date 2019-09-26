3f17c81d1b722b4d7a240c48012eac24e3fc9bb8
tlfobe <lennyfobe@gmail.com>
2019-09-25 23:27:16 -0600

    added test_list_empty_list + code

--- math_lib.py
+++ math_lib.py
@@ -3,7 +3,13 @@ import sys
 
 def list_mean(L):
     if L is None:
-        raise TypeError("Please supply a list!")
+        raise TypeError("list_mean : Please supply a list!")
+
+    if len(L) == 0:
+        raise IndexError("list_mean : Unpopulated list!")
+
+    
+
 
 
 def list_stdev(L):
--- test_math_lib.py
+++ test_math_lib.py
@@ -3,8 +3,14 @@ import math_lib
 
 
 class TestMathLib(unittest.TestCase):
-    def test_list_mean_empty_list(self):
+    def test_list_mean_null(self):
         self.assertRaises(TypeError,
                           math_lib.list_mean,
-                          None
+                          None,
+                          )
+
+    def test_list_mean_empty_list(self):
+        self.assertRaises(IndexError,
+                          math_lib.list_mean,
+                          [],
                           )
